60ec45b07b3eea7b9bece1b130e9fbeb227e17da
Laurence Burden <laurenceburden@gmail.com>
2026-07-03 09:27:12 -0500

    astroids now split depending on size when shot

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
-from constants import LINE_WIDTH
+import random
+from logger import log_event
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
 
 
@@ -12,4 +14,32 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
 
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        rand_angle = random.uniform(20,50)
+            
+        new_roid_velocity1 = self.velocity.rotate(rand_angle)
+        new_roid_velocity2 = self.velocity.rotate(-rand_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        new_roid1 = Asteroid(
+                self.position.x, 
+                self.position.y,
+                new_radius
+        )
+        new_roid1.velocity = new_roid_velocity1 * 1.2
+        
+
+        new_roid2 = Asteroid(
+            self.position.x,
+            self.position.y,
+            new_radius
+        )
+        new_roid2.velocity = new_roid_velocity2 * 1.2
